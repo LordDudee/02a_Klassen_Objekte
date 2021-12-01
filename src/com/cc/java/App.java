36a06516cd93257829c0d2ac8ee2e83b0809fbed
Dude <michael_runschke@gmx.de>
2021-12-01 15:12:49 +0100

    Cat.java hinzugefügt

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -4,7 +4,9 @@ public class App {
     
     public static void main(String[] args) {
 
-    
+        Cat cat = new Cat(); // Was erzeugen wir hier eigentlich? 985.655.350
+        System.out.println(cat);
+
     }
 
 }
