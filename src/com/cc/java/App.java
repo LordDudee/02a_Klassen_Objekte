d9f9e717fde72fe00844ea4828a05deb90a8f402
Dude <michael_runschke@gmx.de>
2021-12-02 14:24:21 +0100

    Info: Typkonvertierung

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -4,7 +4,7 @@ public class App {
     
     public static void main(String[] args) {
 
-        Cat cat1 = new Cat("Grizabella", "white", 29); // Klasse --> Objekt / Instanz   // Referenzvariable = cat1 
+        Cat cat1 = new Cat("Grizabella", "white", 22); // Klasse --> Objekt / Instanz   // Referenzvariable = cat1 
         
         output("Blick von aussen: " + cat1);
         output("RAM-Adresse: " + cat1.getAddress());
@@ -14,7 +14,7 @@ public class App {
 
         output("------------------------");
 
-        Cat cat2 = new Cat("Alonzo", "black", 35); // Klasse --> Objekt / Instanz     // Referenzvariable = cat1 
+        Cat cat2 = new Cat("Alonzo", "black", 27); // Klasse --> Objekt / Instanz     // Referenzvariable = cat1 
         // cat2.setName("#?!3$");
         
         output("Blick von aussen: " + cat2);
@@ -22,6 +22,13 @@ public class App {
         output("Name: " + cat2.getName());
         output("Fellfarbe: " + cat2.getFurColor());
         output("Alter: " + cat2.getAge());
+
+        output ("------Info-------");
+        // output (2); // Typ muss konvertiert werden!
+        output("Zahl: " + 2);
+        output(String.valueOf(2));  // int --> String
+        output(Integer.toString(2));   // int --> String
+
     }
 
     public static void output(String outputStr){
