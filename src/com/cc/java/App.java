6f03f63fa6b84aa524e8438b5e5f27e372bc7540
Dude <michael_runschke@gmx.de>
2021-12-02 11:11:37 +0100

    Wertzuweisung von außen & von innen

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -4,10 +4,27 @@ public class App {
     
     public static void main(String[] args) {
 
-        Cat cat = new Cat(); // Referenzvariable = cat
-        System.out.println("Blick von außen: " + cat);
+        Cat cat1 = new Cat(); // Klasse --> Objekt/Instanz (cat1)  //Referenzvariable = cat1
+        System.out.println("Blick von außen: " + cat1);
+        cat1.name = "Grizabella";
+        cat1.age = 22;
+        cat1.furColor = "white";
+        cat1.tellYourAddress();
+        System.out.println(cat1.name);
+        System.out.println(cat1.age);
+        System.out.println(cat1.furColor);
 
-        cat.tellYourAddress();
+        System.out.println("----------------------------");
+
+        Cat cat2 = new Cat(); // Klasse --> Objekt/Instanz (cat2) //Referenzvariable = cat2
+        System.out.println("Blick von außen: " + cat2);
+        cat2.name = "Alonzo";
+        cat2.age = 24;
+        cat2.tellYourAddress();
+        cat2.furColor = "black";
+        System.out.println(cat2.name);
+        System.out.println(cat2.age);
+        System.out.println(cat2.furColor);
     }
     
 }
