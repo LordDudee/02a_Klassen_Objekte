0253aa799566ead8e644c73954e1136acf971aa7
Dude <michael_runschke@gmx.de>
2021-12-02 08:59:43 +0100

    Referenzvariable und Instanzvariable

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -4,9 +4,10 @@ public class App {
     
     public static void main(String[] args) {
 
-        Cat cat = new Cat(); // Was erzeugen wir hier eigentlich? 985.655.350
-        System.out.println(cat);
+        Cat cat = new Cat(); // Referenzvariable = cat
+        System.out.println("Blick von außen: " + cat);
 
+        cat.tellYourAddress();
     }
-
+    
 }
--- src/com/cc/java/Cat.java
+++ src/com/cc/java/Cat.java
@@ -2,6 +2,8 @@ package com.cc.java;
 
 public class Cat {
     
-    
+    public void tellYourAddress(){
+        System.out.println("Blick von innen: " + this);  // Instanzvariable = this
+    }
 
 }
